904c0161658bf7f78d9db1c2f00178b5fd65fead
Vanessa <vankawa@gmail.com>
2022-06-17 20:08:30 -0300

    Criando primeiro arquivo.

---
+++ UseAcademyFinalProject/UseAcademyFinalProject/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  UseAcademyFinalProject
+//
+//  Created by VanKawa on 17/06/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+
+
+
+}
+
